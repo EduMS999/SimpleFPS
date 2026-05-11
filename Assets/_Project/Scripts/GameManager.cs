6c85c13edf5b1d01f5eaddf4e00af2e44ea27bcf
EduMS999 <eduardomorenosegura@gmail.com>
2026-05-11 21:30:16 +0200

    Empezando con power ups

--- Assets/_Project/Scripts/GameManager.cs
+++ Assets/_Project/Scripts/GameManager.cs
@@ -3,7 +3,25 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager Instance { get; private set; }
+
+    [Header("Referencias UI")]
     public TextMeshProUGUI waveNumberText;
+
+    [Header("Power Ups")]
+    public 
+
+    private void Awake()
+    {
+        // Lógica para asegurar que solo exista una instancia (Singleton)
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject); // Si ya hay uno, destruimos el duplicado
+            return;
+        }
+
+        Instance = this;
+    }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
